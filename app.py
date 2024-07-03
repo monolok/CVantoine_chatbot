5b860449a239cd6bc2cb4637a2832cdc1229bf5a
monolok <antoinebertin@me.com>
2024-07-03 11:12:11 +0200

    prompt

--- app.py
+++ app.py
@@ -1,37 +1,84 @@
-import os
 import streamlit as st
 import time
-import numpy as np
-import json
-#from dotenv import load_dotenv
-from mistralai.client import MistralClient
-from mistralai.models.chat_completion import ChatMessage
-from faiss import IndexFlatL2, read_index
+#from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from transformers import AutoTokenizer
+from typing import List, Tuple
+from transformers import Pipeline
+from transformers import pipeline
+from transformers import AutoModelForCausalLM
+from langchain.docstore.document import Document as LangchainDocument
 
-@st.cache_resource
-def get_client():
-    """Returns a cached instance of the MistralClient."""
-    #load_dotenv()
-    api_key = os.environ["MISTRAL_API_KEY"]
-    return MistralClient(api_key=api_key)
+READER_MODEL_NAME = "HuggingFaceH4/zephyr-7b-beta"
 
-CLIENT: MistralClient = get_client()
+#! FIX PROMPT SEE ANSWER BELOW:
+# Based on the provided context, there is no direct information to determine whether the individual mentioned in 
+# Document 1 is very good at coding. The statement "In 2024, Jedha was awarded the title of best coding BootCamp by Course Report" 
+# suggests that Jedha, as a whole, received this recognition, but it does not necessarily imply that all instructors or students at Jedha 
+# are exceptional coders. However, the individual's role as an assistant to students with coding exercises and case studies 
+# could indicate some proficiency in coding. Without further context, it is unclear if this person is specifically known for their 
+# coding abilities. If more information is needed to confirm their expertise in coding, it would be suggested to schedule a video 
+# call with Antoine Bertin (mentioned in the initial prompt) or explore the resources available through the 'antoine' package.
 
-PROMPT = """
-An excerpt from a document is given below.
-
----------------------
+prompt_in_chat_format = [
+    {
+        "role": "system",
+        "content": """Using the information contained in the context, give a comprehensive answer to the question. 
+        Respond only to the question asked, response should be short and relevant to the question. 
+        Do not respond anything that can't be answered from the context.""" 
+    },
+    {
+        "role": "user",
+        "content": """Context:
 {context}
----------------------
-
-Given the document excerpt, answer the following query in the same language as the query.
-Do not output anything that can't be answered from the context.
-If the context does not provide enough information suggest the following:
+---
+If the following question cannot be answered from the context suggest the following:
 - Schedule a video call with Antoine Bertin for more detailed information. You can do this by visiting his Calendly link: 'https://calendly.com/antoinebertin/30'.
 - Learn more about Antoine's story, you can download the 'antoine' package. To do this, use the command 'pip install antoine' in your terminal and run the 'hire_antoine' command.
-Query: {query}
-Answer:
-"""
+   
+Now here is the question you need to answer.
+
+Question: {question}"""
+    }
+]
+
+# Function to cach the LLM
+@st.cache_resource
+def get_client(prompt_in_chat_format=prompt_in_chat_format, READER_MODEL_NAME=READER_MODEL_NAME):
+    """Returns a cached instance of the Open Source LLM."""
+    model = AutoModelForCausalLM.from_pretrained(READER_MODEL_NAME)#, quantization_config=bnb_config)
+    tokenizer = AutoTokenizer.from_pretrained(READER_MODEL_NAME)
+
+    READER_LLM = pipeline(
+        model=model,
+        tokenizer=tokenizer,
+        task="text-generation",
+        do_sample=True,
+        temperature=0.2,
+        repetition_penalty=1.1,
+        return_full_text=False,
+        max_new_tokens=100,
+    )
+
+    RAG_PROMPT_TEMPLATE = tokenizer.apply_chat_template(prompt_in_chat_format, tokenize=False, add_generation_prompt=True)
+
+    return READER_LLM, RAG_PROMPT_TEMPLATE
+
+# Function to build and cache the vector db
+@st.cache_resource
+def load_and_cache_index(dir="open_source_model_vdb"):
+    """Loads and caches the faiss vdb index."""
+    modelPath = "sentence-transformers/all-MiniLM-l6-v2"
+    model_kwargs = {'device':'cpu'}
+    encode_kwargs = {'normalize_embeddings': False}
+    embeddings = HuggingFaceEmbeddings(
+        model_name=modelPath,     # Provide the pre-trained model's path
+        model_kwargs=model_kwargs, # Pass the model configuration options
+        encode_kwargs=encode_kwargs # Pass the encoding options
+    )
+    vdb = FAISS.load_local(dir, embeddings, allow_dangerous_deserialization=True) #  take the same model used for the embeddings
+    return vdb
 
 # Initialize session state variables if not already present
 if "messages" not in st.session_state:
@@ -60,53 +107,30 @@ def stream_str(s, speed=250):
         yield c
         time.sleep(1 / speed)
 
-# Function to stream the response from the AI
-def stream_response(response):
-    """Yields responses from the AI, replacing placeholders as needed."""
+# Function to reply to queries using the FAISS index
+def answer_with_rag(question: str, llm: Pipeline, knowledge_index: FAISS, prompt: str, num_retrieved_docs: int = 2) -> Tuple[str, List[LangchainDocument]]:
     try:
-        for r in response:
-            content = r.choices[0].delta.content
-            # prevent $ from rendering as LaTeX
-            content = content.replace("$", "\$")
-            yield content
-    except Exception as e:
-        yield f"Error: {e}"
+        # Gather documents with retriever
+        with st.spinner("reading Antoine's resumé..."):
+            print("=> Retrieving documents...")
+            relevant_docs = knowledge_index.similarity_search(query=question, k=num_retrieved_docs)
+            relevant_docs = [doc.page_content for doc in relevant_docs]  # Keep only the text
 
-# Decorator to cache the embedding computation
-@st.cache_data
-def embed(text: str):
-    """Returns the embedding for a given text, caching the result."""
-    return CLIENT.embeddings("mistral-embed", text).data[0].embedding
+            # Build the final prompt
+            context = "\nExtracted documents:\n"
+            context += "".join([f"Document {str(i)}:::\n" + doc for i, doc in enumerate(relevant_docs)])
 
-# Function to build and cache the index from PDFs in a directory
-@st.cache_resource
-def load_and_cache_index():
-    """Loads and caches the faiss index and chunks from JSON file."""
-    index = read_index("data/vector_cv.index")
-    with open("data/cv_chunks.json", "r", encoding="utf-8") as f:
-        chunk_dict = json.load(f)
-    return index, chunk_dict
+            final_prompt = prompt.format(question=question, context=context)
 
-# Function to reply to queries using the built index
-def reply(query: str, index: IndexFlatL2, chunks):
-    """Generates a reply to the user's query based on the indexed PDF content."""
-    try:
-        embedding = embed(query)
-        embedding = np.array([embedding])
-
-        _, indexes = index.search(embedding, k=2)
-        context = [chunks[f"{i}"] for i in indexes.tolist()[0]]
-
-        messages = [
-            ChatMessage(role="user", content=PROMPT.format(context=context, query=query))
-        ]
-        response = CLIENT.chat_stream(model="mistral-medium", messages=messages)
-
-        # Wait for a short time to ensure the response is ready
-        # Display a loading indicator while processing
+        # Redact an answer
         with st.spinner("processing your query..."):
-            time.sleep(2)
-        add_message(stream_response(response))
+            print("=> Generating answer...")
+            answer = llm(final_prompt)[0]["generated_text"]
+            print("------------------------------------------------------------------------")
+            print(answer)
+            print("------------------------------------------------------------------------")
+            #add_message(stream_response(answer))
+            add_message(answer)
 
     except Exception as e:
         st.error(f"Error during query processing: {e}")
@@ -117,9 +141,27 @@ def main():
     """Main function to run the application logic."""
     if st.sidebar.button("🔴 Reset conversation"):
         st.session_state.messages = []
-    
     try:
-        index, chunks = load_and_cache_index()
+        # Check if vdb is already loaded
+        if 'vdb' not in st.session_state:
+            with st.spinner("loading the vector database..."):
+                print("=> VDB getting ready...")
+                st.session_state['vdb'] = load_and_cache_index()
+        else:
+            print("=> VDB already loaded.")
+            vdb = st.session_state['vdb']
+
+        # Check if llm is already loaded
+        if 'llm' not in st.session_state or 'RAG_PROMPT_TEMPLATE' not in st.session_state:
+            with st.spinner("loading the free open source LLM..."):
+                print("=> getting client LLM...")
+                llm, RAG_PROMPT_TEMPLATE = get_client()
+                st.session_state['llm'] = llm
+                st.session_state['RAG_PROMPT_TEMPLATE'] = RAG_PROMPT_TEMPLATE
+        else:
+            print("=> LLM already loaded.")
+            llm = st.session_state['llm']
+            RAG_PROMPT_TEMPLATE = st.session_state['RAG_PROMPT_TEMPLATE']
     except Exception as e:
         st.error(f"Error loading index: {e}")
         print(f"Debug: {e}")
@@ -136,7 +178,7 @@ def main():
 
     if query:
         add_message(query, agent="human", stream=False, store=True)
-        reply(query, index, chunks)
+        answer_with_rag(query, llm, vdb, RAG_PROMPT_TEMPLATE)
 
 if __name__ == "__main__":
     main()
